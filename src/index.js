d39372814e113370dfe4fc3165258e55e5d54db6
marco-amorim <marcoamorim95@hotmail.com>
2020-07-13 02:16:21 -0300

    :rocket: refactoring

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const client = new Discord.Client();
 
 client.once('ready', () => {
-  console.log('bot ready!');
+  console.log('Marco Bot is ready!');
 });
 
 client.on('message', (msg) => {
