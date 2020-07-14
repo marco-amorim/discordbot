ff77f9ee88f59df40aa80daeb2fb43400a9ee6c2
marco-amorim <marcoamorim95@hotmail.com>
2020-07-13 23:16:49 -0300

    :rocket: watch party

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const Discord = require('discord.js');
+
 require('dotenv').config();
 
 const client = new Discord.Client();
 
 client.once('ready', () => {
-  console.log('Marco Bot is ready!');
+  console.log('MarcoBot is Ready!');
 });
 
-client.on('message', (msg) => {
-  console.log(msg);
+client.on('message', async (msg) => {
+  if (msg.guild.id === process.env.GUILD_ID && msg.channel.id === process.env.CHANNEL_ID) {
+    // TODO: watch party link for youtube videos
+    if (msg.content.substr(0, 7) === 'mbwatch') {
+      const msgContent = msg.content.substr(8, 999);
+      const youtube = 'youtube';
+      const prefixParam = 'sync-';
+      const response = msgContent.replace(youtube, prefixParam + youtube);
+      await msg.reply(response);
+      console.log('reply sent!');
+    }
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
